a6b3bea68ec2362c163ece4ec7d8e72d215b9541
cbaker1 <cbaker1@unimelb.edu.au>
2020-08-08 18:29:59 +1000

    Added numerical calculation of total number infected (R infinity)

--- ODE_Models.py
+++ ODE_Models.py
@@ -1,2 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.optimize import fsolve
+
+class SIR_model:
+    def __init__(self, beta=2., gamma=1., s0=.9999, i0=.0001):
+        self.R0 = beta/gamma
+        self.beta = beta
+        self.gamma = gamma
+        self.s0 = s0
+        self.i0 = i0
+        self.r0 = 1 - s0 - i0
+        self.IC = [s0, i0, self.r0]
+
+        self.t = None
+        self.state = None
+
+    def ode_rhs(self, y, t):
+        s, i, r = y
+        return [-self.beta*i*s,
+                self.beta*i*s - self.gamma*i,
+                self.gamma*i]
+
+    def solve_system(self, t_end=10):
+        return odeint(self.ode_rhs, self.IC,
+                      t=np.linspace(0,t_end,100))
+
+    def est_total_infected(self):
+        if self.R0 < 1:
+            return 0
+        def fun(r_inf):
+            return r_inf - (1 - np.exp(-self.R0*r_inf))
+        return fsolve(fun, np.array(0.5))[0]
+if __name__ == "__main__":
+    model = SIR_model(beta=1.1, gamma=1)
+    plt.plot(model.solve_system(t_end=20))
+    plt.show()
+    print(model.R0)
+    print(model.est_total_infected())
+    beta_array = np.linspace(0,2.53,50)
+    total_inf = []
+    for beta_value in beta_array:
+        total_inf.append(SIR_model(beta=beta_value, gamma=1).est_total_infected())
+    plt.plot(beta_array, total_inf)
+    plt.show()
